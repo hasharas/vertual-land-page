322594347156392df24c54486a4c55d1bf842904
hasharas <hasharamsankalpam@gmail.com>
2025-03-12 01:55:23 +0530

    Err:still try to fix that

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import Logo from '../assets/logo.png';
+import { navItems } from "../constants/index";
 
 const Navbar = () => {
       return (
             <div>
-                  <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
-                        <div className=" container px-4 mx-auto relative text-sm">
+                  <nav className="sticky top-[0px] z-50 py-[12px] backdrop-blur-lg border-b border-neutral-700/80">
+                        <div className=" container px-[4px] mx-auto relative text-[14px]">
                               <div className="flex justify-center item-center">
-                                    <div className="flex item-center flex-shrink-0">
-                                          <img className="h-10 w-10 mr-2" src={Logo} alt="logo" />
-                                          <span className="text-xl tracking-tight">VR-Land</span>
+                                    <div className="flex items-center flex-shrink-0">
+                                          <img className="h-[40px] w-[40px] mr-[8px]" src={Logo} alt="logo" />
+                                          <span className="text-[20px] tracking-tight">VR-Land</span>
                                     </div>
+                                    <ul className='hidden lg:flex ml-14 space-x-12'>
+                                          {navItems.map((item, index) => (
+                                                <li key={index}>
+                                                      <a href={item.href}>{item.label}</a>
+
+                                                </li>
+                                          ))
+
+                                          }
+
+                                    </ul>
+
                               </div>
+
+
                         </div>
                   </nav>
             </div>
