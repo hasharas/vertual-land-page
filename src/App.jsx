b532752fc3262a1b5a4479c07eda1d20ad7cdc2b
hasharas <hasharamsankalpam@gmail.com>
2025-03-13 00:36:30 +0530

    Add:hedding text add feture section to

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './component/Navbar';
 import Hero from './component/Hero';
+import Feature from './component/Feature';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <Hero />
+        <Feature />
       </div>
     </>
   );
