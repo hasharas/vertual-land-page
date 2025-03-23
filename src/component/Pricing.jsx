ce43457cd9b00f51bbb1d7f0c3bef56bb8f4cfc3
hasharas <hasharamsankalpam@gmail.com>
2025-03-24 00:38:17 +0530

    complete:pricing section

--- src/component/Pricing.jsx
+++ src/component/Pricing.jsx
@@ -21,8 +21,23 @@ const Pricing = () => {
                                                 )}
                                           </p>
                                           <p className='mb-8'>
-
+                                                <span className='text-5xl mt-6 mr-2'>{option.price}</span>
+                                                <span className='text-neutral-400 tracking-tighter'>/Month</span>
                                           </p>
+                                          <ul >
+                                                {option.features.map((feture, index) => (
+                                                      <li key={index} className='mt-8 flex items-center'>
+
+                                                            <CheckCircle2 />
+                                                            <span className='ml-2'>{feture} </span>
+                                                      </li>
+                                                ))}
+                                          </ul>
+                                          <a href="#" className='inline-flex justify-center items-center text-center w-full h-12 p-5 mt-20 tracking-tight text-xl 
+                                          hover:bg-orange-900 border border-orange-900 rounded-lg transition duration-200'>
+                                                Subscribe
+
+                                          </a>
                                     </div>
                               </div>
                         ))}
